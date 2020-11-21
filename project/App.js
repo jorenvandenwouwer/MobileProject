b0bbcd26468ceb2228825c590652f22a3f7580a3
JorenVanDenWouwer <Joren.vandenwouwer@gmail.com>
2020-11-21 14:17:45 +0100

    tab navigation

--- project/App.js
+++ project/App.js
@@ -1,15 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer} from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+
+const Tab = createBottomTabNavigator();
+export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Map" component={MapScreen}/>
+        <Tab.Screen name="List" component={ListScreen}/>
+        <Tab.Screen name="Favorites" component={FavoriteScreen}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
+const MapScreen = () => {
+  return (
+    <View>
+      <Text>mapview komt hier</Text>
+    </View>
+  )
+}
+const ListScreen = () => {
+  return (
+    <View>
+      <Text>listview komt hier</Text>
+    </View>
+  )
+}
+const FavoriteScreen = () => {
+  return (
+    <View>
+      <Text>
+
+      </Text>
+    </View>
+  )
+}
+
 
 const styles = StyleSheet.create({
   container: {
