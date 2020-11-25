8399d1dcb75035977cdeb9397d85959c28c27f1f
JorenVanDenWouwer <Joren.vandenwouwer@gmail.com>
2020-11-25 16:34:12 +0100

    list view zonder styling

--- project/App.js
+++ project/App.js
@@ -1,34 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer} from '@react-navigation/native';
 import { Button,StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
 import fetch from 'node-fetch';
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
 
 
 const Tab = createBottomTabNavigator();
 export default () => {
+  
   return (
     <NavigationContainer>
       <Tab.Navigator>
+      <Tab.Screen name="List" component={ListScreen}/>
         <Tab.Screen name="Map" component={MapScreen}/>
-        <Tab.Screen name="List" component={ListScreen}/>
         <Tab.Screen name="Favorites" component={FavoriteScreen}/>
       </Tab.Navigator>
     </NavigationContainer>
   )
 }
 
-export const FetchData = () => {
-  const url = "https://api.jsonbin.io/b/5fba7557a825731fc0a11e8b";
-  fetch(url)
-  .then(response => response.json())
-  .then(dataResponse => console.log(dataResponse))
-}
 
 
-export default function App() {
+
+const MapScreen = () => {
+  
   const region = {
     latitude: 51.231107,
     longitude: 4.415127,
@@ -48,15 +46,45 @@ export default function App() {
     </View>
   )
 }
-const ListScreen = () => {
+
+const ListScreen = ({navigation}) => {
+  const [locaties,setLocaties] = useState([]); 
+  useEffect(() => {
+    const FetchData = async() => {
+      try {
+        let respone = await fetch("https://api.jsonbin.io/b/5fbb8b2b522f1f0550cc78f9");
+        let json = await respone.json();
+        setLocaties(json.features);
+      } catch(error){
+        console.log(error)
+      }
+    }
+    FetchData()
+  }, [])
+  const renderItem = ({item}) => {
+    return(
+      <TouchableOpacity onPress={() => navigation.navigate('locatieDetail', {item:item})}>
+        <View style={{backgroundColor:"#4287f5", height:70, margin:1}}><Text>{item.attributes.Naam} {"\n"}{item.attributes.Gemeente} </Text></View>
+      </TouchableOpacity>
+    )
+  }
+  const keyExtractor = (item) => item.attributes.GISID;
+  
   return (
     <View>
-      <Text>listview komt hier</Text>
+      <FlatList
+        data={locaties}
+        keyExtractor= {keyExtractor}
+        renderItem= {renderItem}
+        />
+        <Text> testing</Text>
     </View>
   )
 }
 
-
+const locatieDetail = () => {
+  
+}
 const FavoriteScreen = () => {
   return (
     <View>
